54e8c35905dea0a04ee21a4c65702b4545da6dad
Tal <talsmd95@gmail.com>
2025-07-02 01:06:35 +0300

    Create effect and content to About page

--- Scripts/ui.js
+++ Scripts/ui.js
@@ -2,3 +2,18 @@
 function toggleMenu() {
   document.getElementById('side-menu').classList.toggle('open');
 }
+
+// Fading effect
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeEls = document.querySelectorAll(".scroll-fade");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, { threshold: 0.1 });
+
+  fadeEls.forEach(el => observer.observe(el));
+});
